bc91e2e4c4db6b975997de44eba07eb8e43c1237
seemasahuu <seemakumariteli1910@gmail.com>
2024-06-07 14:47:36 +0530

    ui fixes

--- src/components/Formm.jsx
+++ src/components/Formm.jsx
@@ -13,7 +13,7 @@ export const Formm = ({ visibale, useexit }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30  backdrop-blur-sm lg:flex justify-center items-center">
-      <div className="bg-white h-80 w-80 flex justify-center items-center relative rounded-lg mx-14 my-72">
+      <div className="bg-white h-80 w-80 flex justify-center items-center relative rounded-lg mx-12 my-64">
       <button onClick={useexit} className="absolute top-3 right-3"><IoMdClose className="h-5 w-5"/></button>
       <button
         className="h-10 w-60 bg-slate-100 rounded-md"
