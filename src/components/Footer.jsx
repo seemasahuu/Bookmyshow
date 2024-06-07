7eecc08d16040076bbfb228c829f3237977bf591
seemasahuu <seemakumariteli1910@gmail.com>
2024-06-07 22:00:14 +0530

    ui fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,17 +7,17 @@ import { MdUnsubscribe } from "react-icons/md";
 export const Footer = () => {
   return (
     <div>
-        <div className='h-56 w-full bg-gray-800 hidden lg:block'>
+        <div className='h-56 w-full bg-gray-800'>
             <div className='h-24 w-full flex justify-center items-center text-white'>
-                <h1 className='font-bold text-sm px-32 pr-0'>List your show </h1>,
-                <h2>Got a show, event, activity or a great experince? partner with us & get listed on </h2>,
-                <p className='font-bold text-sm'>BookMyShow</p>
-                <button className='bg-red-400 h-10 w-32 rounded-sm mx-60'>{seema("contact today")}</button>
+                <h1 className='font-bold lg:text-sm text-xs px-32 pr-0 hidden lg;block'>List your show </h1>
+                <h2 className='hidden lg:block'>Got a show, event, activity or a great experince? partner with us & get listed on </h2>
+                <p className='font-bold text-sm mr-6 text-red-400 lg:text-white'>BookMyShow</p>
+                <button className='bg-red-400 h-10 w-32  rounded-sm lg:mx-60'>{seema("contact today")}</button>
             </div>
-            <div className='h-28 bg-slate-400 flex gap-28'>
-                <div className='h-full w-56 mx-40 mr-0 flex flex-col justify-center items-center '><RiCustomerService2Fill  className="h-14 w-16"/> <p className=' '>24/7 Customer care service</p></div>
-                <div className='h-full w-64 flex flex-col justify-center items-center'><FaMoneyBillWave  className="h-14 w-14"/><p className='text-sm '>RESEND BOOKING CONFIRMATION</p></div>
-                <div className='h-full w-56 flex flex-col justify-center items-center'><MdUnsubscribe  className="h-14 w-14"/><p className='text-sm '>SUBSCRIBE TO THE NEWSLETER</p></div>
+            <div className='h-28 bg-slate-400 flex lg:gap-28'>
+                <div className='h-full w-56 lg:mx-40 mr-0 flex flex-col justify-center items-center '><RiCustomerService2Fill  className="h-14 w-16 mr-3"/> <p className='text-xs px-3 lg:text-sm'>24/7 Customer care service</p></div>
+                <div className='h-full w-64 flex flex-col justify-center items-center'><FaMoneyBillWave  className="h-14 w-14 mr-3"/><p className='text-xs lg:text-sm px-3'>RESEND BOOKING CONFIRMATION</p></div>
+                <div className='h-full w-56 flex flex-col justify-center items-center'><MdUnsubscribe  className="h-14 w-14 mr-3"/><p className='text-xs lg:text-sm px-3 '>SUBSCRIBE TO THE NEWSLETER</p></div>
             </div>
         </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ export const Sidebar = ({vissibal,offvisible}) => {
   return (
     <div>
           <div className="fixed inset-0 bg-black bg-opacity-30  backdrop-blur-sm" onClick={offvisible}>
-          <div className="bg-white h-full lg:w-80 w-56 rounded-sm absolute top-0 right-0">
+          <div className="bg-white h-full w-80  rounded-sm absolute top-0 right-0">
             <h1 className='font-bold text-xl p-5'>HEY!</h1>
             <hr/>
             </div> 
