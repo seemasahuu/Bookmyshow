785ad39114da019f396a158fb987a7cbbdc94add
seemasahuu <seemakumariteli1910@gmail.com>
2024-06-07 22:04:18 +0530

    ui fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,9 +15,9 @@ export const Footer = () => {
                 <button className='bg-red-400 h-10 w-32  rounded-sm lg:mx-60'>{seema("contact today")}</button>
             </div>
             <div className='h-28 bg-slate-400 flex lg:gap-28'>
-                <div className='h-full w-56 lg:mx-40 mr-0 flex flex-col justify-center items-center '><RiCustomerService2Fill  className="h-14 w-16 mr-3"/> <p className='text-xs px-3 lg:text-sm'>24/7 Customer care service</p></div>
-                <div className='h-full w-64 flex flex-col justify-center items-center'><FaMoneyBillWave  className="h-14 w-14 mr-3"/><p className='text-xs lg:text-sm px-3'>RESEND BOOKING CONFIRMATION</p></div>
-                <div className='h-full w-56 flex flex-col justify-center items-center'><MdUnsubscribe  className="h-14 w-14 mr-3"/><p className='text-xs lg:text-sm px-3 '>SUBSCRIBE TO THE NEWSLETER</p></div>
+                <div className='h-full w-56 lg:mx-40 mr-0 flex flex-col justify-center items-center '><RiCustomerService2Fill  className="h-9 w-9 lg:h-14 lg:w-16 mr-3"/> <p className='text-xs px-3 lg:text-sm'>24/7 Customer care service</p></div>
+                <div className='h-full w-64 flex flex-col justify-center items-center'><FaMoneyBillWave  className="h-9 w-9 lg:h-14 lg:w-14 mr-3"/><p className='text-xs lg:text-sm px-3'>RESEND BOOKING CONFIRMATION</p></div>
+                <div className='h-full w-56 flex flex-col justify-center items-center'><MdUnsubscribe  className="h-9 w-9 lg:h-14 lg:w-14 mr-3"/><p className='text-xs lg:text-sm px-3 '>SUBSCRIBE TO THE NEWSLETER</p></div>
             </div>
         </div>
     </div>
