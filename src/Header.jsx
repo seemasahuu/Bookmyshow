1fbf39c74dc5da85d7a4c9c0d70775998f9bb195
seemasahuu <seemakumariteli1910@gmail.com>
2024-06-07 10:49:11 +0530

    ui fixes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosMenu } from "react-icons/io";
 import { Button } from "./Button";
 import { Sidebar } from "./Sidebar";
 
 export const Header = () => {
+  const [sidbar, setsidbar] = useState(false);
+
+  const handleSlidbar = () => {
+    setsidbar(true);
+  };
+
+  const slidbarexit = () =>{
+    setsidbar(false);
+  }
   return (
     <div className="h-20 lg:h-16 flex items-center ">
       <div className="hidden lg:block lg:w-28 lg:h-full"></div>
@@ -14,20 +23,6 @@ export const Header = () => {
           alt=""
           className="lg:h-10 lg:w-28 h-8"
         />
-
-{/* <div className="w-24 block lg:hidden">
-        <select
-          id="country"
-          name="country"
-          autocomplete="country-name"
-          className=" w-full rounded-md border-0 py-1.5 text-gray-900 outline-none  border-none        sm:text-sm "
-        >
-          <option>Jaipur</option>
-          <option>Delhi</option>
-          <option>Goa</option>
-        </select>
-      </div> */}
-
       </div>
       <input
         type="text"
@@ -52,9 +47,9 @@ export const Header = () => {
       <Button />
 
       <div className="w-16 h-6 ">
-        <IoIosMenu className="h-full w-16" />
+        <IoIosMenu className="h-full w-16" onClick={handleSlidbar} />
       </div>
-      <Sidebar/>
+      <Sidebar vissibal={sidbar} offvisible={slidbarexit}/>
     </div>
   );
 };
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export const Sidebar = () => {
+export const Sidebar = ({vissibal,offvisible}) => {
+    if (!vissibal) return null;
   return (
     <div>
-          <div className="fixed inset-0 bg-black bg-opacity-30  backdrop-blur-sm flex justify-center items-center">
-
-            
+          <div className="fixed inset-0 bg-black bg-opacity-30  backdrop-blur-sm" onClick={offvisible}>
+          <div className="bg-white h-full w-80 flex justify-center items-center rounded-sm absolute top-0 right-0"></div> 
           </div>
     </div>
   )
