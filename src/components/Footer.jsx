a9e94b374422ab8d98f3a887834036b727f12a89
seemasahuu <seemakumariteli1910@gmail.com>
2024-06-07 22:59:24 +0530

    function ui fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export const Footer = () => {
                 <h1 className='font-bold lg:text-sm text-xs px-32 pr-0 hidden lg;block'>List your show </h1>
                 <h2 className='hidden lg:block'>Got a show, event, activity or a great experince? partner with us & get listed on </h2>
                 <p className='font-bold text-sm mr-6 text-red-400 lg:text-white'>BookMyShow</p>
-                <button className='bg-red-400 h-10 w-32  rounded-sm lg:mx-60'>{seema("contact today")}</button>
+                <button className='bg-red-400 h-10 w-32 rounded-md lg:rounded-sm lg:mx-60'>{seema("contact today")}</button>
             </div>
             <div className='h-28 bg-slate-400 flex lg:gap-28'>
                 <div className='h-full w-56 lg:mx-40 mr-0 flex flex-col justify-center items-center '><RiCustomerService2Fill  className="h-9 w-9 lg:h-14 lg:w-16 mr-3"/> <p className='text-xs px-3 lg:text-sm'>24/7 Customer care service</p></div>
---
+++ src/components/slide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const slide = () => {
+  return (
+    <div>slide</div>
+  )
+}
+
+export default slide
